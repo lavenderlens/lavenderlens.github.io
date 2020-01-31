22369ec8e8f1512b412c130c632a89a6d933eeea
Alan Lavender <alanlavendersmv@gmail.com>
2020-01-31 11:03:34 +0000

    Add files via upload

---
+++ precache-manifest.d8cd5b507e6890e97ccf66ac3668b368.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "907963939569c8ac4a67",
+    "url": "/static/css/main.eb944104.chunk.css"
+  },
+  {
+    "revision": "907963939569c8ac4a67",
+    "url": "/static/js/main.90796393.chunk.js"
+  },
+  {
+    "revision": "72ff7299dd5b90c494ad",
+    "url": "/static/js/1.72ff7299.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "96f6e8ca72ece1adbf1c5934efaaf6db",
+    "url": "/static/media/clef.96f6e8ca.svg"
+  },
+  {
+    "revision": "7ab4ad1bd1df2ca1a241b44de627773f",
+    "url": "/index.html"
+  }
+];
